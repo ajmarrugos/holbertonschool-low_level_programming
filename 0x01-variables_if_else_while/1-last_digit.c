cfaa4b94308e4abeb623b3ff3e35b66a4ca8999a
Alberto Marrugo <ajmarrugos@gmail.com>
2022-02-11 10:59:04 -0800

    Last digit fixed for 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,11 +13,11 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 ld = n % 10;
 
-if (ld > 5)
-printf("Last digit of %d is %d and is greater than 5\n", n, ld);
+if (ld == 0)
+printf("Last digit of %d is %d and is 0\n", n, ld);
 else if (ld < 6)
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 else
-printf("Last digit of %d is %d and is 0\n", n, ld);
+printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 return (0);
 }
