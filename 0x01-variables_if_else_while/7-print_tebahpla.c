06262d22ec9c5d9c0512fdf94a6da808dd818262
Alberto Marrugo <ajmarrugos@gmail.com>
2022-02-11 13:06:23 -0800

    za

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char za;
+for (za = 'z'; za >= 'a'; za--)
+putchar(za);
+putchar('\n');
+return (0);
+}
