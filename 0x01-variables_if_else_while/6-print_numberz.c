6976915095c832fc2b7d8c1204d014dbafb92876
Alberto Marrugo <ajmarrugos@gmail.com>
2022-02-11 12:58:57 -0800

    NuMbErZ

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+int num;
+for (num = 0; num < 10; num++)
+putchar(num + '0');
+putchar('\n');
+
+return (0);
+}
