a88ea0e8d4d5ea555a4599a86dbf21b01f9c4ddb
Alberto Marrugo <ajmarrugos@gmail.com>
2022-02-15 12:30:19 -0800

    Fixed trailing space in task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 /**
 * jack_bauer - Prints every minute of the day of Jack Bauer
 * Contains a cicle and a concatenation of characters
-* Return: Minutes and hours between 00:00 - 23:59 
+* Return: Minutes and hours between 00:00 - 23:59
 **/
 void jack_bauer(void)
 {
