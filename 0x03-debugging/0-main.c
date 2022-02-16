de8908069260872753f1bc651ccd33a7ca938576
Alberto Marrugo <ajmarrugos@gmail.com>
2022-02-16 07:59:03 -0800

    New Debugging Repo + Readme + Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - Tests function: positive_or_negative
+ * Return: 0
+ **/
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,3 @@
+# Debugging in C #
+
+This library is for learning how to debug in C.
