04b4538e841e516d624c9a697853b975f32fc4cc
Alberto Marrugo <ajmarrugos@gmail.com>
2022-02-18 09:03:20 -0800

    Fixed main

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,12 +6,9 @@
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
-<<<<<<< HEAD
 int _mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
-=======
 int mul(int a, int b);
->>>>>>> 75352e96df9131af26dd173e996402e172c1f3a5
 
 #endif /* MAIN_H */
