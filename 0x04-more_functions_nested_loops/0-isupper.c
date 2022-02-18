5c41285903954e71f8c2091476a07a56d7bc0c13
Alberto Marrugo <ajmarrugos@gmail.com>
2022-02-18 08:03:34 -0800

    Task 0 fix + Task 1 test

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -16,3 +16,4 @@ else
 {
 return (0);
 }
+}
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _isdigit - Checks if digit
+* @c: Input integer to check
+* Return: 1 if 'c' is digit or 0 if else.
+**/
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
