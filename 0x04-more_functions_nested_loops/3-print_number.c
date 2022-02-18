f3cd0bd1443e4a3f96e94a9d99cbc19035a9a20e
Alberto Marrugo <ajmarrugos@gmail.com>
2022-02-18 08:59:03 -0800

    Printers + main update

---
+++ 0x04-more_functions_nested_loops/3-print_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_numbers - Prints numbers from 0 to 9
+* Return: numbers since 0 to 9
+**/
+void print_numbers(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+_putchar(c);
+_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_most_numbers - Print the numbers since 0 up to 9
+* The function excludes '2' and '4' numbers
+* Return: The numbers since 0 up to 9 without '2' or '4'
+**/
+void print_most_numbers(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+if (c != '2' && c != '4')
+_putchar(c);
+_putchar('\n');
+}
