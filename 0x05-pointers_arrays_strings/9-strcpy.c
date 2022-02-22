c3dafca94c948759cb312b3f2d46ae4a07e060dc
Alberto Marrugo <ajmarrugos@gmail.com>
2022-02-21 20:03:36 -0800

    StrCopy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,10 +9,11 @@
 
 char *_strcpy(char *dest, char *src)
 {
-int i = 0;
-for (; src[i] != '\0'; i++)
+int i;
+for (i = 0; src[i] != '\0'; i++)
 {
 dest[i] = src[i];
 }
+dest[i++] = '\0';
 return (dest);
 }
