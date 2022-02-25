1819be17600f9b0e6a8b21818869944cc175bcb8
Alberto Marrugo <ajmarrugos@gmail.com>
2022-02-24 20:55:17 -0800

    ROT13 + main update

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* rot13 - Entry point
+* one if, two loops only...
+* @n: String to be encoded
+* Return: Decrypted string
+**/
+
+char *rot13(char *n)
+{
+int x, rot_c = 13, i = 0;
+char toswap[] = {'A', 'N', 'a', 'n', 'B', 'O', 'b', 'o', 'C', 'P',
+	'c', 'p', 'D', 'Q', 'd', 'q', 'E', 'R', 'e', 'r', 'F', 'S', 'f',
+	's', 'G', 'T', 'g', 't', 'H', 'U', 'h', 'u', 'I', 'V', 'i', 'v',
+	'J', 'W', 'j', 'w', 'K', 'X', 'k', 'x', 'L', 'Y', 'l', 'y', 'M',
+	'Z', 'm', 'z'};
+
+while (n[i] != '\0')
+{
+for (x = 0; x <= 51; x++)
+{
+if (n[i] == toswap[x])
+{
+n[i] = n[i] + rot_c;
+x = 51;
+}
+rot_c = rot_c * -1;
+}
+i++;
+}
+return (n);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,5 +12,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *n);
 char *leet(char *n);
+char *rot13(char *s);
 
 #endif /* MAIN_H */
