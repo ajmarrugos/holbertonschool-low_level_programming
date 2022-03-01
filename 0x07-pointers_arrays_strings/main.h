808fc473320f8619b930d88b3cceac8461fe0663
Alberto Marrugo <ajmarrugos@gmail.com>
2022-02-28 19:38:01 -0800

    Main update

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,8 +9,8 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+int _putchar(char c);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-int _putchar(char c);
 
 #endif /* MAIN_H */
