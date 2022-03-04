249c0cf6856db66d2bd6c230a4169de546de1cc5
Alberto Marrugo <ajmarrugos@gmail.com>
2022-03-04 10:31:14 -0800

    How many arguments?

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#included "main.h"
+
+/**
+* main - Will print the number of args
+* @argc: Argument count
+* @argv: Argument vector
+* Return: 0
+**/
+
+int main(int argc, char *argv[])
+{
+(void) argv;
+printf("%d\n", argc - 1);
+return (0);
+}
