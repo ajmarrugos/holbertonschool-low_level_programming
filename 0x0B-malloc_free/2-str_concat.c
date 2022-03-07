2c05f4459bd7f1388160ea51e9385fb7391d7f0e
Alberto Marrugo <ajmarrugos@gmail.com>
2022-03-07 12:10:09 -0800

    Task 2 - Wrong code fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,36 +1,41 @@
 #include "main.h"
 
 /**
-* *string_nconcat - Concatenates n bytes of a string to another string
+* *str_concat - Concatenates n bytes of a string to another string
 * @s1: string to append to
 * @s2: string to concatenate from
 * @n: Number of bytes from s2 to concatenate to s1
 * Return: Pointer to the resulting string
 **/
 
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *str_concat(char *s1, char *s2)
 {
+int i = 0, x = 0, y = 0, z = 0;
 char *s;
-unsigned int i = 0, x = 0, len1 = 0, len2 = 0;
-while (s1 && s1[len1])
-len1++;
-while (s2 && s2[len2])
-len2++;
-if (n < len2)
-s = malloc(sizeof(char) * (len1 + n + 1));
-else
-s = malloc(sizeof(char) * (len1 + len2 + 1));
-if (!s)
-return (NULL);
-while (i < len1)
-{
-s[i] = s1[i];
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+while (s1[i])
 i++;
+while (s2[x])
+x++;
+z = i + x;
+s = malloc((sizeof(char) * z) +1);
+if (s == NULL)
+return (NULL);
+x = 0;
+while (y < z)
+{
+if (y <= i)
+s[y] = s1[y];
+if (y >= i)
+{
+s[y] = s2[x];
+x++;
 }
-while (n < len2 && i < (len1 + n))
-s[i++] = s2[x++];
-while (n >= len2 && i < (len1 + len2))
-s[i++] = s2[x++];
-s[i] = '\0';
+y++;
+}
+s[y] = '\0';
 return (s);
 }
