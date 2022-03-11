3fe4a54694c41e9daab854004e7db1c578850368
Alberto Marrugo <ajmarrugos@gmail.com>
2022-03-11 07:07:56 -0800

    Malloc fixed

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,39 +1,16 @@
 #include "main.h"
 
 /**
-* *string_nconcat - Will concatenate two strings
-* @s1: First string
-* @s2: Second string
-* @n: Limit of s2
-* Return: Pointer to new space in memory or null
+* *malloc_checked - Will allocate memory using malloc
+* @b: Number of bytes to allocate
+* Return: A pointer to the allocated memory
 **/
 
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *malloc_checked(unsigned int b)
 {
-char *strDup;
-int i;
-unsigned int x;
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-i = 0;
-while (s1[i] != '\0')
-i++;
-strDup = malloc(sizeof(char) * (i + n + 1));
-if (strDup == NULL)
-return (NULL);
-i = x = 0;
-while (s1[i] != '\0')
-{
-strDup[i] = s1[i];
-i++;
-}
-while (x < n && s2[x] != '\0')
-{
-strDup[i] = s2[x];
-i++, x++;
-}
-strDup[i] = '\0';
-return (strDup);
+void *ptr;
+ptr = malloc(b);
+if (ptr == NULL)
+exit(98);
+return (ptr);
 }
