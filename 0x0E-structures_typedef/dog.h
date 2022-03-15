bbd34eb4e17a2c91c083e487526ba6746de69043
Alberto Marrugo <ajmarrugos@gmail.com>
2022-03-14 19:38:39 -0700

    Dog love

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,7 +6,6 @@
 * @name: The name of the dog.
 * @age: The age of the dog.
 * @owner: The owner of the dog.
-* Description: The attributes of a dog.
 **/
 
 struct dog
@@ -16,12 +15,7 @@ float age;
 char *owner;
 };
 
-/**
-* dog_t - Typedef for dog structure
-**/
-
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
