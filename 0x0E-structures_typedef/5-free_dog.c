e94139af3e3f8d44f7b6058bc0c8b735bf3fe835
Alberto Marrugo <ajmarrugos@gmail.com>
2022-03-14 20:40:38 -0700

    Who let the dogs out?

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+* free_dog - function that frees dogs
+* @d: pointer
+* Return: 0
+**/
+
+void free_dog(dog_t *d)
+{
+if (d)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
