af101efdbe547a093d886844fe129c3e95038275
Alberto Marrugo <ajmarrugos@gmail.com>
2022-03-17 15:31:41 -0700

    GCC Function Definition Fix & Returnings

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,37 +2,71 @@
 #include "3-calc.h"
 
 /**
-* op_add - Returns the sum of two numbers.
-* op_sub - Returns the difference of two numbers.
-* op_mul - Returns the product of two numbers.
-* op_div - Returns the division of two numbers.
-* op_mod - Returns the remainder of the division of two numbers. 
-* @a: input one
-* @b: input two
-* Return: The output of the given operation
+* op_add - adds two numbers
+* @a: first number
+* @b: second number
+* Return: sum of a + b
 **/
 
 int op_add(int a, int b)
 {
-	return (a + b);
+return (a + b);
 }
+
+/**
+* op_sub - substract two numbers
+* @a: first number
+* @b: second number
+* Return: substraction of a - b
+**/
 
 int op_sub(int a, int b)
 {
-	return (a - b);
+return (a - b);
 }
+
+/**
+* op_mul - multiplies two numbers
+* @a: first number
+* @b: second number
+* Return: product of a * b
+**/
 
 int op_mul(int a, int b)
 {
-	return (a * b);
+return (a * b);
 }
+
+/**
+* op_div - divides two numbers
+* @a: first number
+* @b: second number
+* Return: division of a / b
+**/
 
 int op_div(int a, int b)
 {
-	return (a / b);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
 }
+return (a / b);
+}
+
+/**
+* op_mod - rest of the division of two numbers
+* @a: first number
+* @b: second number
+* Return: rest of the division of a / b
+**/
 
 int op_mod(int a, int b)
 {
-	return (a % b);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a % b);
 }
