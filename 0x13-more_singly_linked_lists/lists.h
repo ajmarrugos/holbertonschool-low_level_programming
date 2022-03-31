3728f84e753eb92438cb9ecd47d0529f3b698217
Alberto Marrugo <ajmarrugos@gmail.com>
2022-03-31 08:27:24 -0700

    More singly linked lists Project

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef _LISTS_
+#define _LISTS_
+
+/*  STD LIBRARIES */
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* listint_s - Singly linked list
+* @n: Integer argument
+* @next: Points to the next node
+* Description: Singly linked list node structure
+**/
+
+typedef struct listint_s
+{
+int n;
+struct listint_s *next;
+} listint_t;
+
+/* PROTOTYPES */
+
+size_t print_listint(const listint_t *h);
+
+#endif
