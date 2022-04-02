cb31f4fc521be8cf64130374fc7642a9bc0aecdb
Alberto Marrugo <ajmarrugos@gmail.com>
2022-04-01 21:39:43 -0700

    Task 8: Done & tested + header update

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,15 +1,14 @@
 #include "lists.h"
 
 /**
- * sum_listint - returns the sum of all the data (n) of
- * a linked list.
- * @head: head of a list.
- *
- * Return: sum of all the data (n).
- */
+* sum_listint - Returns the sum of all the data of a list
+* @head: head of a list.
+* Return: sum of all the data (n).
+**/
+
 int sum_listint(listint_t *head)
 {
-  int sum;
+int sum;
 sum = 0;
 while (head != NULL)
 {
