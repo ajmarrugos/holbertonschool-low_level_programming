b2b8b6c7a52b083e4235ff1b457cf5778437dc99
ajmarrugos <ajmarrugos@gmail.com>
2022-04-05 16:46:37 -0500

    Task 2: Function done & tested + header update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,9 +10,9 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-
 /* PROTOTYPES */
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
