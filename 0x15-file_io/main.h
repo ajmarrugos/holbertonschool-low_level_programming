c7e231a0adeec5264a666f776b3684b789ed2089
Alberto Marrugo <ajmarrugos@gmail.com>
2022-04-05 10:00:57 -0700

    Task 1: Function done & tested + header update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,8 +7,12 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 
 /* PROTOTYPES */
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
