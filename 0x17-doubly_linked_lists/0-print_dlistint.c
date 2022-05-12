319d8da9a5f8d265b094f58ccac80f7d814b029e
ajmarrugos <ajmarrugos@gmail.com>
2022-05-12 12:45:17 -0500

    Betty sign

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,12 +8,12 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i = 0;
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		i++;
-	}
-	return (i);
+size_t i = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+i++;
+}
+return (i);
 }
