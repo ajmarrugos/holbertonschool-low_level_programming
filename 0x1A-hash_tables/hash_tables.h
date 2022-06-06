b96276e8d87fc5308093534dae6f9971dc6a6974
ajmarrugos <ajmarrugos@gmail.com>
2022-06-06 13:49:49 -0500

    Implement djb2 algorithm

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -33,6 +33,8 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
+/* HASH FUNCTIONS */
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 
 #endif
