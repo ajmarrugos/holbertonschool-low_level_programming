250f59720bf56212532718b59bb328ecb7c1a387
ajmarrugos <ajmarrugos@gmail.com>
2022-08-11 20:18:39 -0500

    First sorting algos

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+
+/**
+* linear_search - function that searches for a value in an array of integers
+* using the Linear search algorithm
+* @array: Pointer to the first element of the array to search in
+* @size: Number of elements in array
+* @value: Value to search for
+* Return: The first index where value is located or -1 if error
+**/
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (!array || !value || !size)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
